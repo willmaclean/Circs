2eae799264cf3bc6470ab7549e77caa880e9cf85
Will Maclean <willmaclean@gmail.com>
2021-01-13 14:59:48 +0000

    updated tests

---
+++ tests/tests.py
@@ -0,0 +1,31 @@
+import unittest
+import pandas as pd
+from network import Pairs, find_subgraphs
+
+class TestPairs(unittest.TestCase):
+
+	def setUp(self):
+		#load test data
+		self.df_ = pd.read_pickle('transactions_core_monthly_grouped.pkl')
+
+	def test_input(self):
+		with self.assertRaises(KeyError):
+			self.df_[['to', 'from', 'amount']]
+
+	def test_pairs(self):
+		Pairs.fit(self.df_)
+		pairs = pairs.get_pair_transactions(self.df_)
+		pair_ids = Pairs.pair_ids_
+		self.assertEqual(len(pair_ids), pairs.shape[1])
+
+class Testsubgraph(unittest.TestCase):
+
+	def setUp(self):
+		self.df_ = pd.read_pickle('transactions_core_monthly_grouped.pkl')
+
+	def test_graph(self):
+		G = find_subgraphs(self.df_)
+		self.assertFalse(networkx.is_directed(G))
+
+		
+
